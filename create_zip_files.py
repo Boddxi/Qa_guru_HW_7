c70b50ec5994ec359f18f7b236298fd3bfb17429
Boddxi <boddxi@yandex.ru>
2025-09-18 22:04:18 +0300

    Update and rename create_and_read_zip_files.py to create_zip_files.py

---
+++ create_zip_files.py
@@ -0,0 +1,13 @@
+from zipfile import ZipFile
+import os.path
+
+
+if not os.path.exists("resources"):
+    os.mkdir("resources")
+zip_path = os.path.join("resources", "documents.zip")
+files = [os.path.join("File_folder", "File_pdf.pdf"), os.path.join("File_folder", "File_xls.xlsx"), os.path.join("File_folder", "File_csv.csv")]
+with  ZipFile(zip_path, 'w') as zip_file:
+    for file in files:
+        zip_file.write(file)
+print("Список доступных файлов в архиве:")
+print(zip_file.namelist())
