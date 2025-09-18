0e39cf9cefa421084814957cce51d15ede807dfd
Juliya Smikova <boddxi@yandex.ru>
2025-09-18 21:53:17 +0300

    Some of the homework is ready2

--- test_checking_the_contents_of_zip_some_files.py
+++ test_checking_the_contents_of_zip_some_files.py
@@ -1,14 +1,14 @@
 from zipfile import ZipFile
 import PyPDF2
 from io import BytesIO
-
+import os.path
 
 
 def test_checking_the_contents_of_zip_pdf_file():
-    with ZipFile('resources/documents.zip') as zip_file:
+    with ZipFile(os.path.abspath('resources/documents.zip')) as zip_file:
         print("Список доступных файлов в архиве:")
         print(zip_file.namelist())
-        with zip_file.open('File_pdf.pdf') as pdf_file:
+        with zip_file.open('File_folder/File_pdf.pdf') as pdf_file:
             # Создаем PdfReader из бинарных данных
             pdf_reader = PyPDF2.PdfReader(BytesIO(pdf_file.read()))
             print("Первая страница файла содержит текст:")
