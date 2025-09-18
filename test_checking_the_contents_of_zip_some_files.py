4587d62a57f8bb41956681997ee1831d16a2c780
Juliya Smikova <boddxi@yandex.ru>
2025-09-18 19:28:20 +0300

    Some of the homework is ready

---
+++ test_checking_the_contents_of_zip_some_files.py
@@ -0,0 +1,17 @@
+from zipfile import ZipFile
+import PyPDF2
+from io import BytesIO
+
+
+
+def test_checking_the_contents_of_zip_pdf_file():
+    with ZipFile('resources/documents.zip') as zip_file:
+        print("Список доступных файлов в архиве:")
+        print(zip_file.namelist())
+        with zip_file.open('File_pdf.pdf') as pdf_file:
+            # Создаем PdfReader из бинарных данных
+            pdf_reader = PyPDF2.PdfReader(BytesIO(pdf_file.read()))
+            print("Первая страница файла содержит текст:")
+            text= pdf_reader.pages[0].extract_text()
+            print(text)
+            assert "PBR руководство от Allegorithmic" in text
