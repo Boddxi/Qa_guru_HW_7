8a8322d4f8f86e1740ced21813587f91948e4ddb
Juliya <julieta-petrova@yandex.ru>
2025-09-21 11:58:04 +0300

    errors have been fixed

--- create_zip_files.py
+++ create_zip_files.py
@@ -1,16 +1,20 @@
 import zipfile
-from zipfile import ZipFile
 import os.path
 
+def create_zip_files():
+    if not os.path.exists("resources"):
+        os.mkdir("resources")
 
-if not os.path.exists("resources"):
-    os.mkdir("resources")
-zip_path = os.path.join("resources", "documents.zip")
-files = [os.path.join("File_folder", "File_pdf.pdf"), os.path.join("File_folder", "File_xls.xlsx"), os.path.join("File_folder", "File_csv.csv")]
+    zip_path = os.path.join("resources", "documents.zip")
+    files = [os.path.join("File_folder", "File_pdf.pdf"), os.path.join("File_folder", "File_xls.xlsx"), os.path.join("File_folder", "File_csv.csv")]
+
+    with zipfile.ZipFile(zip_path, 'x', compression=zipfile.ZIP_DEFLATED) as zip_file:
+        for file in files:
+            add_file_path = os.path.join(zip_path, file)
+            zip_file.write(file, os.path.basename(file))
+            print(f'Путь к файлу в архиве: {add_file_path}')
 
 
-with zipfile.ZipFile(zip_path, 'w', compression=zipfile.ZIP_DEFLATED) as zip_file:
-    for file in files:
-        add_file_path = os.path.join(zip_path, file)
-        zip_file.write(file)
-        print(add_file_path)
+create_zip_files()
+
+
--- test_checking_the_contents_of_zip_some_files.py
+++ test_checking_the_contents_of_zip_some_files.py
@@ -1,36 +1,37 @@
 from zipfile import ZipFile
-from pypdf import PdfReader
-import csv
 import PyPDF2
 from io import BytesIO
 import pandas as pd
 from io import TextIOWrapper
-import os.path
 
 
-def test_checking_the_contents_of_zip_pdf_csv_xlsx_files():
+def test_checking_the_contents_of_zip_pdf_files():
     with ZipFile('resources/documents.zip') as zip_file:
-        print("Список доступных файлов в архиве:")
-        print(zip_file.namelist())
-        with zip_file.open('File_folder/File_pdf.pdf') as pdf_file:
+        with zip_file.open('File_pdf.pdf') as pdf_file:
             # Создаем PdfReader из бинарных данных
             reader_pdf = PyPDF2.PdfReader(BytesIO(pdf_file.read()))
             print("Первая страница файла содержит текст:")
             text_pdf= reader_pdf.pages[0].extract_text()
             print(text_pdf)
             assert "PBR руководство от Allegorithmic" in text_pdf
-        with zip_file.open('File_folder/File_xls.xlsx') as xlsx_file:
+
+
+def test_checking_the_contents_of_zip_xlsx_files():
+    with ZipFile('resources/documents.zip') as zip_file:
+        with zip_file.open('File_xls.xlsx') as xlsx_file:
             reader_xlsx = pd.read_excel(xlsx_file.read())
             print("Содержимое файла XlSX:")
             print(reader_xlsx)
             assert 'ширина' in reader_xlsx.columns
             assert 'Runflat' in reader_xlsx['опции'].values
-        with zip_file.open('File_folder/File_csv.csv') as csv_file:
+
+
+def test_checking_the_contents_of_zip_csv_files():
+    with ZipFile('resources/documents.zip') as zip_file:
+        with zip_file.open('File_csv.csv') as csv_file:
             reader_csv = pd.read_csv(TextIOWrapper(csv_file, encoding='cp1251'))
             print("Содержимое файла CSV:")
             print(reader_csv)
             column_names = list(reader_csv.columns)
             print(column_names)
             assert ['Статус;Дата;Комментарий;Серийный номер;Обработка'] == column_names
-
-
