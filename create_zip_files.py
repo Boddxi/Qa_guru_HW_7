3b485d2e3dcfedbb60150fca340aa00a0828f5ae
Juliya <julieta-petrova@yandex.ru>
2025-09-21 12:21:08 +0300

    errors have been fixed

--- create_zip_files.py
+++ create_zip_files.py
@@ -11,8 +11,6 @@ def create_zip_files():
     with zipfile.ZipFile(zip_path, 'x', compression=zipfile.ZIP_DEFLATED) as zip_file:
         for file in files:
             add_file_path = os.path.join(zip_path, file)
-            zip_file.write(file, os.path.basename(file))
-            print(f'Путь к файлу в архиве: {add_file_path}')
 
 
 create_zip_files()
--- test_checking_the_contents_of_zip_some_files.py
+++ test_checking_the_contents_of_zip_some_files.py
@@ -1,15 +1,33 @@
 from zipfile import ZipFile
-import PyPDF2
 from io import BytesIO
 import pandas as pd
 from io import TextIOWrapper
+import zipfile
+import os.path
+from pypdf import PdfReader
+
+
+
+def create_zip_files():
+    if not os.path.exists("resources"):
+        os.mkdir("resources")
+
+    zip_path = os.path.join("resources", "documents.zip")
+    files = [os.path.join("File_folder", "File_pdf.pdf"), os.path.join("File_folder", "File_xls.xlsx"), os.path.join("File_folder", "File_csv.csv")]
+
+    with zipfile.ZipFile(zip_path, 'w', compression=zipfile.ZIP_DEFLATED) as zip_file:
+        for file in files:
+            zip_file.write(file, os.path.basename(file))
+
+
+create_zip_files()
 
 
 def test_checking_the_contents_of_zip_pdf_files():
     with ZipFile('resources/documents.zip') as zip_file:
         with zip_file.open('File_pdf.pdf') as pdf_file:
             # Создаем PdfReader из бинарных данных
-            reader_pdf = PyPDF2.PdfReader(BytesIO(pdf_file.read()))
+            reader_pdf = PdfReader(BytesIO(pdf_file.read()))
             print("Первая страница файла содержит текст:")
             text_pdf= reader_pdf.pages[0].extract_text()
             print(text_pdf)
@@ -19,7 +37,7 @@ def test_checking_the_contents_of_zip_pdf_files():
 def test_checking_the_contents_of_zip_xlsx_files():
     with ZipFile('resources/documents.zip') as zip_file:
         with zip_file.open('File_xls.xlsx') as xlsx_file:
-            reader_xlsx = pd.read_excel(xlsx_file.read())
+            reader_xlsx = pd.read_excel(BytesIO(xlsx_file.read()))
             print("Содержимое файла XlSX:")
             print(reader_xlsx)
             assert 'ширина' in reader_xlsx.columns
